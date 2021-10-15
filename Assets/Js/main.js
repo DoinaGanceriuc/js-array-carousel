2cb72d80138381e1f4bfbceb75052f85d7f8403a
DoinaGanceriuc <doinaganceriuc@gmail.com>
2021-10-15 15:59:51 +0200

    add basic for
    aggiunto ciclo for

--- Assets/Js/main.js
+++ Assets/Js/main.js
@@ -14,13 +14,13 @@ una lista ordinata di 5 immagini,
 una lista ordinata dei relativi 5 luoghi e
 una lista di 5 news, */
 const items = [
-    'img/01.jpg',
-    'img/02.jpg',
-    'img/03.jpg',
-    'img/04.jpg',
-    'img/05.jpg'
+    './img/01.jpg',
+    './img/02.jpg',
+    './img/03.jpg',
+    './img/04.jpg',
+    './img/05.jpg'
 ];
-console.log(items);
+
 
 const title = [
     'Svezia',
@@ -29,7 +29,7 @@ const title = [
     'Germania',
     'Paradise'
 ];
-console.log(title);
+
 
 const text = [
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
@@ -38,8 +38,47 @@ const text = [
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
 ];
-console.log(text);
 
 // mostrare immagine a video
 let jumboElement = document.getElementById("jumbo");
-/* jumboElement.innerHTML = items[0]; */
+jumboElement.src = items[0];
+
+let imgOneElement = document.getElementById("lake");
+imgOneElement.src = items[0];
+
+
+let imgTwoElement = document.getElementById("sweden");
+imgTwoElement.src = items[1];
+
+let imgThreeElement = document.getElementById("london");
+imgThreeElement.src = items[2];
+
+let imgFourElement = document.getElementById("city");
+imgFourElement.src = items[3];
+
+let imgFiveElement = document.getElementById("paradise");
+imgFiveElement.src = items[4];
+
+let imagesElement = [jumboElement, imgOneElement, imgTwoElement, imgThreeElement, imgFourElement, imgFiveElement];
+
+
+
+
+document.getElementById("up").addEventListener("click", function(){
+
+    console.log("cliccato");
+
+
+
+     for (let i = 0; i< imagesElement.length; i++) {
+        const singleImage = imagesElement[i];
+        console.log(singleImage);
+    
+}
+     
+    /*  document.getElementById("jumbo").src = singleImg;*/
+    
+     /* console.log(singleImg);  */
+     
+ }
+)
