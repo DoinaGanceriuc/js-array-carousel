fc2f93da6b79dda7b2dc4db6723bc065e50176de
DoinaGanceriuc <doinaganceriuc@gmail.com>
2021-10-15 11:54:19 +0200

    initial commit

---
+++ Assets/Js/main.js
@@ -0,0 +1,4 @@
+/* Dati tre array contenenti:
+una lista ordinata di 5 immagini,
+una lista ordinata dei relativi 5 luoghi e
+una lista di 5 news, creare un carosello. Al click dell'utente sulle frecce verso l'alto o verso il basso, l'immagine attiva diventa visibile in formato grande a sinistra e nel suo angolo in basso a destra dovranno essere aggiunti i relativi titolo e testo. Allo stesso tempo nelle miniature l'immagine attiva dovrà apparire in evidenza rispetto alle altre. */
